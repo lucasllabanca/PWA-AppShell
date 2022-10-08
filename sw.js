f7431dfb2c2e4efeba6fffa8b826f793dde4b1e7
lucasllabanca <lucasllabanca@hotmail.com>
2022-10-08 14:13:28 -0300

    feat: activating sw

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
 self.addEventListener('install', () => {
-    console.log('[Service Worker] installing service worker...');
-    self.skipWaiting();
+    console.log('[Service Worker] installing service worker');
+    self.skipWaiting(); //no need to wait
+});
+
+self.addEventListener('activate', () => {
+    console.log('[Service Worker] Activating service worker!');
+    return self.clients.claim(); //forces all tabs use this solution
 });
