62bfd72b297b753f16dc8a626c7dff4bd6eb2159
lucasllabanca <lucasllabanca@hotmail.com>
2022-10-08 14:43:38 -0300

    feat: fetch add to sw

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ if ('serviceWorker' in navigator) {
     const onsuccess = () => console.log('[Service Worker] Registered');
     const onerror = () => console.log('[Service Worker] Registration failed');
     
-    navigator.serviceWorker.
-    register('sw.js')
+    navigator.serviceWorker
+    .register('sw.js')
     .then(onsuccess)
     .catch(onerror);
 }
--- sw.js
+++ sw.js
@@ -7,3 +7,8 @@ self.addEventListener('activate', () => {
     console.log('[Service Worker] Activating service worker!');
     return self.clients.claim(); //forces all tabs use this solution
 });
+
+self.addEventListener('fetch', (event) => {
+    console.log('[Service Worker] Fetch event worker', event.request.url);
+    event.respondWith(fetch(event.request.url));
+});
